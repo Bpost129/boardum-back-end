3dc1b7e10dd4f89631fd7ef5da66f1ddb85bb0c8
Bpost129 <Bpost129@yahoo.com>
2026-02-05 14:55:49 -0500

    add index and create routes to boards router, mount to server

---
+++ routes/boards.js
@@ -0,0 +1,11 @@
+import { Router } from "express"
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+import * as boardsCtrl from '../controllers/boards.js'
+
+const router = Router()
+
+router.use(decodeUserFromToken)
+router.get('/', checkAuth, boardsCtrl.index)
+router.post('/', checkAuth, boardsCtrl.create)
+
+export { router }
