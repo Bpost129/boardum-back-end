8eabe76ddfea79a29332ac4cc7200de43d7dc07e
Bpost129 <Bpost129@yahoo.com>
2026-02-06 10:28:07 -0500

    create list index controller function

--- controllers/lists.js
+++ controllers/lists.js
@@ -3,7 +3,9 @@ import { Board } from "../models/board.js"
 
 async function index(req, res) {
   try {
-    
+    const lists = await List.find({})
+      .populate('board')
+    res.status(200).json(lists)
   } catch (err) {
     console.log(err)
     res.status(500).json(err)
