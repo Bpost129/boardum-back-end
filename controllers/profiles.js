9701d1c75a829c909832d4486abb8602186a35ac
Bpost129 <Bpost129@yahoo.com>
2026-02-07 11:40:16 -0500

    add show function to profile controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -11,6 +11,16 @@ async function index(req, res) {
   }
 }
 
+async function show(req, res) {
+  try {
+    const profile = await Profile.findById(req.user.profile)
+    res.status(200).json(profile)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
+
 async function addPhoto(req, res) {
   try {
     const imageFile = req.files.photo.path
